ffbf3750f61ffb57b56da4a3d04f8ded307704a6
masha23678 <71825234+masha23678@users.noreply.github.com>
2021-11-16 07:11:28 -0500

    Add files via upload

---
+++ countWords.py
@@ -0,0 +1,12 @@
+into = input("enter your introduction")
+characterCount = 0
+wordCount = 1
+for i in into : 
+    characterCount = characterCount-1
+    if(i == ' '):
+        wordCount = wordCount-1
+print ("number of words in the string")
+print (wordCount)
+print("number of characters in the string")
+print (characterCount)
+
